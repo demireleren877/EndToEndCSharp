2ebacc2d857c5139dd02af4be57a5a44d77cff7a
eren demirel <l5819033@std.yildiz.edu.tr>
2022-04-29 16:05:44 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+
+ProductManager productManager = new ProductManager(new EFProductDal());
+foreach (var item in productManager.GetAllByUnitPrice(50,100))
+{
+    Console.WriteLine(item.ProductName);
+}
+
---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,60 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Product> _products;
+        public InMemoryProductDal()
+        {
+            //Oracle,Sql Server, Postgres , MongoDb
+            _products = new List<Product> {
+                new Product{ProductId=1, CategoryId=1, ProductName="Bardak", UnitPrice=15, UnitsInStock=15},
+                new Product{ProductId=2, CategoryId=1, ProductName="Kamera", UnitPrice=500, UnitsInStock=3},
+                new Product{ProductId=3, CategoryId=2, ProductName="Telefon", UnitPrice=1500, UnitsInStock=2},
+                new Product{ProductId=4, CategoryId=2, ProductName="Klavye", UnitPrice=150, UnitsInStock=65},
+                new Product{ProductId=5, CategoryId=2, ProductName="Fare", UnitPrice=85, UnitsInStock=1}
+            };
+        }
+
+        public void Add(Product entity)
+        {
+            _products.Add(entity);
+        }
+
+        public void Delete(Product entity)
+        {
+            Product productToDelete = _products.SingleOrDefault(p => p.ProductId == entity.ProductId);
+
+            _products.Remove(productToDelete);
+        }
+
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            return _products;
+        }
+
+
+        public void Update(Product entity)
+        {
+            Product productToUpdate = _products.SingleOrDefault(p => p.ProductId == entity.ProductId);
+            productToUpdate.ProductName = entity.ProductName;
+            productToUpdate.CategoryId = entity.CategoryId;
+            productToUpdate.UnitPrice = entity.UnitPrice;
+            productToUpdate.UnitsInStock = entity.UnitsInStock;
+
+        }
+    }
+}
