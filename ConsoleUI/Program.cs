1298f5486687edb99eff1edab66f6753a0a760d2
eren demirel <l5819033@std.yildiz.edu.tr>
2022-04-29 19:28:43 +0300

    core layer done

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -4,9 +4,23 @@ using Business.Concrete;
 using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 
-ProductManager productManager = new ProductManager(new EFProductDal());
-foreach (var item in productManager.GetAllByUnitPrice(50,100))
+//CategoryTest();
+ProductTest();
+
+static void ProductTest()
 {
-    Console.WriteLine(item.ProductName);
+    ProductManager productManager = new ProductManager(new EFProductDal());
+    foreach (var item in productManager.GetAllByUnitPrice(50, 100))
+    {
+        Console.WriteLine(item.ProductName);
+    }
 }
 
+static void CategoryTest()
+{
+    CategoryManager categoryManager = new CategoryManager(new EFCategoryDal());
+    foreach (var item in categoryManager.GetAll())
+    {
+        Console.WriteLine(item.CategoryName);
+    }
+}
